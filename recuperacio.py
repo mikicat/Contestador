ac30c030974b34bb79ed0f6f53715d6ad689e524
mikicat <mcomash@gmail.com>
2015-08-19 11:16:16 +0200

    Create recuperacio.py

---
+++ recuperacio.py
@@ -0,0 +1,28 @@
+import pickle
+
+def dni():
+    ask = input("El teu D.N.I: ")
+    dni = open('dni.dat', 'wb')
+    pickle.dump(ask, dni)
+    dni.close()
+    contra()
+    
+def contra():
+    ask = input("D.N.I: ")
+    dni = open('dni.dat', 'rb')
+    readni = pickle.load(dni)
+    dni.close()
+    if ask == readni:
+        newpwd = input("Nova Contrassenya: ")
+        repeat = input("Repeteix la Contrassenya: ")
+        if newpwd == repeat:
+            contra = open('contra.dat', 'wb')
+            pickle.dump(newpwd, contra)
+            contra.close()
+        else:
+            print("Acces Denegat")
+    else:
+        print("Acces Denegat")
+
+
+dni()
