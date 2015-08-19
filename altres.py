eac934c21bed180e70f110915d3f8a6b82e14c97
mikicat <mcomash@gmail.com>
2015-08-19 11:14:26 +0200

    Create altres.py

---
+++ altres.py
@@ -0,0 +1,58 @@
+import time
+import sys
+import pickle
+from tkinter import *
+
+def respostes_limitades():
+    print("Whooops!")
+    print("Recorda que tinc respostes limitades")
+
+def read_record():
+    hora = input("Per d'aquí quants minuts? ")
+    num = float(hora)
+    print("\n" * 50)
+    countdown = num * 60
+    while countdown > 0:
+        time.sleep(1)
+        countdown -= 1
+    print("\n")
+    load_file = open('recordatori.dat', 'rb')
+    loaded_recordatori = pickle.load(load_file)
+    load_file.close()
+    tk = Tk()
+    tk.wm_attributes("-topmost", 1)
+    tk.title('Compte endarrere acabat!')
+    Label(tk, text="RECORDA: %s" % loaded_recordatori).grid(row=0, column=0, sticky=W)
+    Button(tk, text="Quit", command=tk.destroy).grid(row=1, column=1, sticky=W)
+
+
+def recorda():
+    print("RECORDA: PERQUE EL COMPTE ENRERE FUNCIONI HA D'ESTAR EL PROGRAMA OBERT, I HAURAS D'EXECUTAR-NE UN ALTRE PER PODER CONTINUAR PARLANT")
+    nota = input("Què vols recordar? ")
+    save_file = open('recordatori.dat', 'wb')
+    pickle.dump(nota, save_file)
+    save_file.close()
+    read_record()
+
+def mira_formulari():
+    load = open('formulari.dat', 'rb')
+    loaded = pickle.load(load)
+    load.close()
+    print(loaded)
+
+def contacta():
+    #formulari
+    nom = input("Nom: ")
+    cognom = input("Cognom(s): ")
+    email = input("Email: ")
+    comentari = input("Comentari: ")
+    dades = {'Nom' : nom,
+    'Cognom' : cognom,
+    'Email' : email,
+    'Comentari' : comentari,
+    }
+    save = open('formulari.dat', 'wb')
+    pickle.dump(dades, save)
+    save.close()
+
+
