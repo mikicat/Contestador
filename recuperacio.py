7f55378560ad337b4573c785990652ae41fe62f6
Miquel Comas <mcomash@gmail.com>
2015-08-20 21:03:37 +0200

    Update recuperacio.py

--- recuperacio.py
+++ recuperacio.py
@@ -1,28 +1,14 @@
 import pickle
 
-def dni():
-    ask = input("El teu D.N.I: ")
-    dni = open('dni.dat', 'wb')
-    pickle.dump(ask, dni)
-    dni.close()
-    contra()
-    
 def contra():
-    ask = input("D.N.I: ")
-    dni = open('dni.dat', 'rb')
-    readni = pickle.load(dni)
-    dni.close()
-    if ask == readni:
-        newpwd = input("Nova Contrasenya: ")
-        repeat = input("Repeteix la Contrasenya: ")
-        if newpwd == repeat:
-            contra = open('contra.dat', 'wb')
-            pickle.dump(newpwd, contra)
-            contra.close()
-        else:
-            print("Les contrasenyes no coincideixen.")
+    newpwd = input("Nova Contrasenya: ")
+    repeat = input("Repeteix la Contrasenya: ")
+    if newpwd == repeat:
+        contra = open('contra.dat', 'wb')
+        pickle.dump(newpwd, contra)
+        contra.close()
     else:
-        print("Acces Denegat")
+        print("Les contrasenyes no coincideixen.")
 
 
 
