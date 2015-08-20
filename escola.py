30e2ed90e95c23bb91021c9b68d18d956158406f
mikicat <mcomash@gmail.com>
2015-08-20 12:45:55 +0200

    Update escola.py

--- escola.py
+++ escola.py
@@ -3,3 +3,5 @@ import webbrowser
 def web_escola():
     webbrowser.open("") # lloc web de l'escola que vulguis
     return True
+def web_estudiants()
+    webbrowser.open("") # lloc web dels estudiants 
