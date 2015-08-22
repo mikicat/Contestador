43f1e04728e33e1edf8eb0541a8bc48953260ef3
Miquel Comas <mcomash@gmail.com>
2015-08-22 20:46:47 +0200

    Syntax Error Corrected

--- escola.py
+++ escola.py
@@ -3,5 +3,5 @@ import webbrowser
 def web_escola():
     webbrowser.open("") # lloc web de l'escola que vulguis
     return True
-def web_estudiants()
+def web_estudiants():
     webbrowser.open("") # lloc web dels estudiants 
