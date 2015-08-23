5459ec25f1d9eba04276395bf549683f4e015fd7
Miquel Comas <mcomash@gmail.com>
2015-08-23 12:59:11 +0200

    Update vida.py

--- vida.py
+++ vida.py
@@ -2,6 +2,10 @@ import random
 import time
 import webbrowser
 
+def saluda():
+    hola = ["Benvingut", "Ei!", "Bon dia", "Allô!", "Eis!", "Hoooola!"]
+    print(random.choice(hola))
+
 def vida_estat():
     estats = ['Estic be', 'Perfecte!', 'Em sento sol', 'Doncs com que plou estic trist', 'Em trobo malament', 'Estic radiant!', 'Estic malalt...', 'Doncs estic adormit', 'No ho se gaire... Be?', 'Despert... El maleït gos ha tornat a bordar a les 6!!!', 'Deixa\'m en pau! Tant t\'avorreixes que has de parlar amb una màquina? No tens vida social o què?',]
     print(random.choice(estats))
