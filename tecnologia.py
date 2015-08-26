a9ed7986a53140fdb444091531a12f8efc0012da
Miquel Comas <mcomash@gmail.com>
2015-08-26 11:38:55 +0200

    Update tecnologia.py

--- tecnologia.py
+++ tecnologia.py
@@ -1,5 +1,11 @@
 import webbrowser
+import time
 
 def noticies():
     webbrowser.open("http://www.324.cat")
     return True
+
+def proposta():
+    print("Proposa-ho!")
+    time.sleep(1.5)
+    webbrowser.open("https://github.com/mikicat/Contestador/issues")
